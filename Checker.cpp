84ad1ec454e1039719675c6bf20b5dab83485956
Vrushali1512 <satawalekarvrushali@gmail.com>
2025-07-20 18:18:08 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,51 +2,38 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool temperature(int x)
+bool Min_Max(int x,int min,int max)
 {
-    if(x < 0 || x > 45)
-    {
-        printf("Temperature out of range\n");
-        return 0;
-    }
-    else
+    if (x>min && x<max)
     return 1;
+    else
+    return 0;
 }
 
-bool StateOfCharge(int x)
+bool battery_Check(bool x, bool y, bool z)
 {
-    if(x < 20 || x > 80)
+    if(x && y && z)
     {
-        printf("State of charge out of range\n");
-        return 0;
-    }
-    else
-    return 1;
-}
-
-bool chargeRate(float x)
-{
-    if(x > 0.8)
-    {
-        printf("Charge Rate out of range!\n");
-        return 0;
-    }
-    else
-    return 1;
-}
-bool batteryIsOk(int x, int y, float z)
-{
-    bool a, b, c;
-    a = temperature(x);
-    b = StateOfCharge(y);
-    c = chargeRate(z);
-    if(a && b && c)
-    {
-        printf("Battery condition ok\n");
+        printf("Battery is Ok\n");
         return 1;
     }
     else
-    return 0;
+    {
+        printf("Battery is not ok\n");
+        return 0;
+    }
+}
+
+bool batteryIsOk(int temp,int soc,int chargeRate)
+{
+    bool x,y,z;
+    x = Min_Max(temp,0,45);
+    y = Min_Max(soc,20,80);
+    if(chargeRate<0.8)
+    z = 1;
+    else
+    z =0;
+    battery_Check(x,y,z);
 }
 
 int main() {
